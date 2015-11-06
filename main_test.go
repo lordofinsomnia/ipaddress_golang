a174bc1e3b3de4a6e245733c93dde3f69251be9e
lordofinsomnia <mickom669@yahoo.com>
2015-11-06 19:58:03 +0100

    BenchmarkPackServiceLink added

--- main_test.go
+++ main_test.go
@@ -9,3 +9,9 @@ func TestPackServiceLink(t *testing.T) {
 		t.Error("PackServiceLink failed expectedValue:" + expectedValue + " got " + realValue)
 	}
 }
+
+//BenchmarkPackServiceLink-8	2000000000	         0.00 ns/op
+func BenchmarkPackServiceLink(b *testing.B) {
+	packServiceLink("192.168.0.1", "http", 80)
+
+}
